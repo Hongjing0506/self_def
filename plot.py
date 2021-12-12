eba6b58c6c37eac89f41c55536be6c807f5bfd61
Hongjing0506 <18520228343@163.com>
2021-12-12 21:50:02 +0800

    sign_check_star modified

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2021-12-12 21:40:15
 LastEditors: ChenHJ
-LastEditTime: 2021-12-12 21:42:43
+LastEditTime: 2021-12-12 21:49:23
 FilePath: /chenhj/self_def/plot.py
 Aim: 
 Mission: 
@@ -33,12 +33,13 @@ from scipy.stats import t
 
 '''
 description: 对于heatmap，输入r和rlim进行显著性检验并对通过显著性检验的值加*号
-param {*} ax 图
-param {*} r 需要做检验的r
-param {*} rlim 检验标准
-param {*} xpos 调整*号的x轴位置
-param {*} ypos 调整*号的y轴位置
-param {*} size 调整*号的大小
+note: xpos和ypos与生成heatmap的坐标轴刻度有关
+param {axs object} ax 图
+param {array} r 需要做检验的r
+param {array} rlim 检验标准
+param {float} xpos 调整*号的x轴位置
+param {float} ypos 调整*号的y轴位置
+param {str} size 调整*号的大小
 return {*}
 '''
 def sign_check_star(ax, r, rlim, xpos, ypos, size):
@@ -48,3 +49,4 @@ def sign_check_star(ax, r, rlim, xpos, ypos, size):
     for i,j in zip(i_pass,j_pass):
         print(i,j)
         ax.text(j+xpos, i+ypos, "*", size=size)
+    del(x, y, i_pass, j_pass, i, j)
