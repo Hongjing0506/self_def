3789275ad503eefd3bb32e6daaca2783d6dac823
Hongjing0506 <18520228343@163.com>
2022-04-26 01:06:07 +0800

    taylor_diagram

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2021-12-12 21:40:15
 LastEditors: ChenHJ
-LastEditTime: 2022-03-18 23:31:57
+LastEditTime: 2022-04-26 01:04:04
 FilePath: /chenhj/self_def/plot.py
 Aim: 
 Mission: 
@@ -180,3 +180,158 @@ def patches(ax, x0, y0, width, height, proj):
         linestyle="--",
     )
     ax.add_patch(rect)
+    
+def taylor_diagram(ax,r,std, dotlabels):
+    if np.min(r)<0:
+        ax.set_thetalim(thetamin=0, thetamax=180)
+        r_small, r_big, r_interval=0,1.5+0.1,0.5  #横纵坐标范围，最小值 最大值 间隔
+        ax.set_rlim(r_small,r_big)
+        rad_list=[-1,-0.99,-0.95,-0.9,-0.8,-0.7,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.7,0.8,0.9,0.95,0.99,1] #需要显示数值的主要R的值
+        # minor_rad_list=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.85,0.86,0.87,0.88,0.89,
+                        # 0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99,1] #需要显示刻度的次要R的值
+        angle_list = np.rad2deg(np.arccos(rad_list))
+        angle_list_rad=np.arccos(rad_list)
+        # angle_minor_list = np.arccos(minor_rad_list)
+        ax.set_thetagrids(angle_list, rad_list)
+        ax.tick_params(pad=8)
+        # lines, labels = plt.thetagrids(angle_list, labels=rad_list, frac=1.25)
+        v = 0.11
+        for i in np.arange(r_small, r_big, r_interval):
+            if i == 1:
+                ax.text(np.arctan(i/v)-0.5*np.pi, np.sqrt(v**2+i**2), s='REF', ha='center', va='top') 
+                ax.text(1.5*np.pi-np.arctan(i/v), np.sqrt(v**2+i**2), s='REF', ha='center', va='top')
+                #text的第一个坐标是角度（弧度制），第二个是距离
+            elif i == 0:
+                ax.text(1.5*np.pi, v, s=str(i), ha='center', va='top')
+            else: 
+                ax.text(np.arctan(i/v)-0.5*np.pi, np.sqrt(v**2+i**2), s=str(i), ha='center', va='top') 
+                ax.text(1.5*np.pi-np.arctan(i/v), np.sqrt(v**2+i**2), s=str(i), ha='center', va='top') 
+        ax.set_rgrids([])
+        labels = ax.get_xticklabels() + ax.get_yticklabels()
+        ax.grid(False)
+        angle_linewidth,angle_length,angle_minor_length=0.8,0.02,0.01
+        tick = [ax.get_rmax(), ax.get_rmax() * (1 - angle_length)]
+        # tick_minor = [ax.get_rmax(), ax.get_rmax() * (1 - angle_minor_length)]
+        for t in angle_list_rad:
+            ax.plot([t, t], tick, lw=angle_linewidth, color="k")  # 第一个坐标是角度（角度制），第二个是距离
+        # for t in angle_minor_list:
+        #     ax.plot([t, t], tick_minor, lw=angle_linewidth, color="k")  # 第一个坐标是角度（角度制），第二个是距离
+
+        # 然后开始绘制以REF为原点的圈，可以自己加圈
+        circle = plt.Circle((1, 0), 0.5, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='gray',linestyle='--', linewidth=0.8)
+        ax.add_artist(circle)
+        circle = plt.Circle((1, 0), 1.0, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='gray',linestyle='--', linewidth=0.8)
+        ax.add_artist(circle)
+
+        # 绘制以原点为圆点的圆圈：
+        circle4 = plt.Circle((0, 0), 0.5, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='grey',linestyle='--', linewidth=1.0)
+        circle5 = plt.Circle((0, 0), 1, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='grey',linestyle='-', linewidth=1.5)
+        circle6 = plt.Circle((0, 0), 1.5, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='grey',linestyle='--', linewidth=1.0)
+        ax.add_artist(circle4)
+        ax.add_artist(circle5)
+        ax.add_artist(circle6)
+
+        #ax.set_xlabel('Normalized')
+        ax.text(np.deg2rad(40), 1.85, s='Correlation', ha='center', va='bottom', rotation=-45)  
+
+        # 这里的网格包括：以原点为圆点的圆圈。首先绘制从原点发散的线段，长度等于半径
+        ax.plot([0,np.arccos(0.4)],[0,3],lw=1,color='gray',linestyle='--')
+        ax.plot([0,np.arccos(0.8)],[0,3],lw=1,color='gray',linestyle='--')
+        ax.plot([0,np.arccos(0.0)],[0,3],lw=1,color='gray',linestyle='--')
+        ax.plot([0,np.arccos(-0.4)],[0,3],lw=1,color='gray',linestyle='--')
+        ax.plot([0,np.arccos(-0.8)],[0,3],lw=1,color='gray',linestyle='--')
+
+        # 画点，参数一相关系数，参数二标准差 
+        for i in np.arange(0,len(r)):
+            ax.plot(np.arccos(r[i]), std[i], 'o',color='r',markersize=0, label='{} {}'.format(i+1,dotlabels[i]))
+            ax.text(np.arccos(r[i]), std[i], s='{}'.format(i+1), c='r',fontsize=10)
+            # ax.text(np.arccos(r[i]-0.05), std[i], s='2', c='r',fontsize=13)
+            # ax.plot(np.arccos(r[0]), std[0], 'o',color='#FF8000',markersize=10, label='1')
+            # ax.text(np.arccos(r[0]-0.05), std[0], s='1', c='#FF8000',fontsize=13)
+        # ax.plot(np.arccos(r[2]), std[2], 'o',color='g',markersize=10, label='3')
+        # ax.text(np.arccos(r[2]-0.05), std[2], s='3',c='g', fontsize=13)
+        # ax.plot(np.arccos(r[3]), std[3], 'o',color='b',markersize=10, label='4')
+        # ax.text(np.arccos(r[3]-0.05), std[3], s='4', c='b',fontsize=13)
+        # ax.plot(np.arccos(r[4]), std[4], 'o',color='#E800E8',markersize=10, label='5')
+        # ax.text(np.arccos(r[4]-0.05), std[4], s='5', c='#E800E8',fontsize=13)
+        # ax.plot(np.arccos(r[5]), std[5], '^',color='#00AEAE',markersize=10, label='6')
+        # ax.text(np.arccos(r[5]-0.05), std[5], s='6', c='#00AEAE',fontsize=13)
+        ax.text(1.5*np.pi, 0.3, s='Std (Normalized)',ha='center', va='top')
+    elif np.min(r)>=0:
+        ax.set_thetalim(thetamin=0, thetamax=90)
+        r_small, r_big, r_interval=0,1.5+0.1,0.5  #横纵坐标范围，最小值 最大值 间隔
+        ax.set_rlim(r_small,r_big)
+        rad_list=[0,0.2,0.4,0.6,0.7,0.8,0.9,0.95,0.99,1] #需要显示数值的主要R的值
+        # minor_rad_list=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.85,0.86,0.87,0.88,0.89,
+                        # 0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99,1] #需要显示刻度的次要R的值
+        angle_list = np.rad2deg(np.arccos(rad_list))
+        angle_list_rad=np.arccos(rad_list)
+        # angle_minor_list = np.arccos(minor_rad_list)
+        ax.set_thetagrids(angle_list, rad_list)
+        
+        #   设置pcc和圆弧的距离
+        ax.tick_params(pad=8)
+        # lines, labels = plt.thetagrids(angle_list, labels=rad_list, frac=1.25)
+        #   画出Std的坐标轴（横向）
+        v = 0.07
+        for i in np.arange(r_small, r_big, r_interval):
+            if i == 1:
+                ax.text(np.arctan(i/v)-0.5*np.pi, np.sqrt(v**2+i**2), s='REF', ha='center', va='top') 
+                ax.text(0.5*np.pi+np.arctan(v/i), np.sqrt(v**2+i**2), s='REF', ha='center', va='top')
+                #text的第一个坐标是角度（弧度制），第二个是距离
+            elif i == 0:
+                ax.text(1.5*np.pi, v, s=str(i), ha='center', va='top')
+                ax.text(np.pi, v, s=str(i), ha='center', va='top')
+            else: 
+                ax.text(np.arctan(i/v)-0.5*np.pi, np.sqrt(v**2+i**2), s=str(i), ha='center', va='top') 
+                ax.text(0.5*np.pi+np.arctan(v/i), np.sqrt(v**2+i**2), s=str(i), ha='center', va='top')
+                # ax.text(1.5*np.pi-np.arctan(i/v), np.sqrt(v**2+i**2), s=str(i), ha='center', va='top') 
+        ax.set_rgrids([])
+        labels = ax.get_xticklabels() + ax.get_yticklabels()
+        ax.grid(False)
+        angle_linewidth,angle_length,angle_minor_length=0.8,0.02,0.01
+        tick = [ax.get_rmax(), ax.get_rmax() * (1 - angle_length)]
+        # tick_minor = [ax.get_rmax(), ax.get_rmax() * (1 - angle_minor_length)]
+        for t in angle_list_rad:
+            ax.plot([t, t], tick, lw=angle_linewidth, color="k")  # 第一个坐标是角度（角度制），第二个是距离
+        # for t in angle_minor_list:
+        #     ax.plot([t, t], tick_minor, lw=angle_linewidth, color="k")  # 第一个坐标是角度（角度制），第二个是距离
+
+        # 然后开始绘制以REF为原点的圈，可以自己加圈
+        circle = plt.Circle((1, 0), 0.5, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='gray',linestyle='--', linewidth=0.8)
+        ax.add_artist(circle)
+        circle = plt.Circle((1, 0), 1.0, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='gray',linestyle='--', linewidth=0.8)
+        ax.add_artist(circle)
+
+        # 绘制以原点为圆点的圆圈：
+        circle4 = plt.Circle((0, 0), 0.5, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='grey',linestyle='--', linewidth=1.0)
+        circle5 = plt.Circle((0, 0), 1, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='grey',linestyle='-', linewidth=1.5)
+        circle6 = plt.Circle((0, 0), 1.5, transform=ax.transData._b, facecolor=(0, 0, 0, 0), edgecolor='grey',linestyle='--', linewidth=1.0)
+        ax.add_artist(circle4)
+        ax.add_artist(circle5)
+        ax.add_artist(circle6)
+
+        #ax.set_xlabel('Normalized')
+        ax.text(np.deg2rad(40), 1.85, s='Correlation', ha='center', va='bottom', rotation=-45)  
+
+        # 这里的网格包括：以原点为圆点的圆圈。首先绘制从原点发散的线段，长度等于半径
+        ax.plot([0,np.arccos(0.4)],[0,3],lw=1,color='gray',linestyle='--')
+        ax.plot([0,np.arccos(0.8)],[0,3],lw=1,color='gray',linestyle='--')
+        
+
+        # 画点，参数一相关系数，参数二标准差 
+        for i in np.arange(0,len(r)):
+            ax.plot(np.arccos(r[i]), std[i], 'o',color='r',markersize=0, label='{} {}'.format(i+1,dotlabels[i]))
+            ax.text(np.arccos(r[i]), std[i], s='{}'.format(i+1), c='r',fontsize=10)
+            # ax.text(np.arccos(r[i]-0.05), std[i], s='2', c='r',fontsize=13)
+            # ax.plot(np.arccos(r[0]), std[0], 'o',color='#FF8000',markersize=10, label='1')
+            # ax.text(np.arccos(r[0]-0.05), std[0], s='1', c='#FF8000',fontsize=13)
+        # ax.plot(np.arccos(r[2]), std[2], 'o',color='g',markersize=10, label='3')
+        # ax.text(np.arccos(r[2]-0.05), std[2], s='3',c='g', fontsize=13)
+        # ax.plot(np.arccos(r[3]), std[3], 'o',color='b',markersize=10, label='4')
+        # ax.text(np.arccos(r[3]-0.05), std[3], s='4', c='b',fontsize=13)
+        # ax.plot(np.arccos(r[4]), std[4], 'o',color='#E800E8',markersize=10, label='5')
+        # ax.text(np.arccos(r[4]-0.05), std[4], s='5', c='#E800E8',fontsize=13)
+        # ax.plot(np.arccos(r[5]), std[5], '^',color='#00AEAE',markersize=10, label='6')
+        # ax.text(np.arccos(r[5]-0.05), std[5], s='6', c='#00AEAE',fontsize=13)
+        ax.set_ylabel('Std (Normalized)',labelpad=35)
