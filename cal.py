8c8c78a01e17495e2f5be7e614f0a891908f9278
Hongjing0506 <18520228343@163.com>
2022-11-04 13:36:19 +0800

    修改了mon_to_season的bug

--- cal.py
+++ cal.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-03-02 16:58:52
 LastEditors: ChenHJ
-LastEditTime: 2022-11-04 12:22:35
+LastEditTime: 2022-11-04 13:06:32
 FilePath: /chenhj/self_def/cal.py
 Aim: 
 Mission: 
@@ -382,7 +382,7 @@ def mon_to_season(data, **kargs):
   args = {"continuous":False, "season":"all"}
   args = {**args, **kargs}
   new_data = data.sel(time=data.time[2:-1]).coarsen(time=3).mean()
-  new_data.coords["time"] = pd.date_range(str(data.time[3].data), str(data.time[-2].data), freq="3MS")
+  new_data.coords["time"] = pd.date_range(str(data.time.dt.year[0].data)+"0401", periods=len(data.time)/3-1, freq="3MS")
   data_MAM = new_data.sel(time=new_data.time.dt.month == 4)
   data_JJA = new_data.sel(time=new_data.time.dt.month == 7)
   data_SON = new_data.sel(time=new_data.time.dt.month == 10)
